e6fab5b8df38227f2d3117891c5bf03585ca88f3
franciscojavierojeda <franciscojavier.ojeda@bosonit.com>
2022-04-26 10:55:23 +0200

    Ejercicio search on type

---
+++ buscaCochesApp/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BuscadorComponent } from './components/buscador/buscador.component';
+import { PaisComponent } from './components/pais/pais.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BuscadorComponent,
+    PaisComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
